fc607368fb120658410a64ed94986e8f52170831
traab <traab@students.cs.ubc.ca>
2021-11-02 21:24:56 -0700

    added graph and trace tabs

--- src/main/java/org/intellij/sdk/toolWindow/MyToolWindow.java
+++ src/main/java/org/intellij/sdk/toolWindow/MyToolWindow.java
@@ -4,8 +4,10 @@ package org.intellij.sdk.toolWindow;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.table.JBTable;
 
 import javax.swing.*;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.util.Calendar;
 
@@ -25,6 +27,10 @@ public class MyToolWindow {
   private JButton undoSlicerButton;
   private JComboBox comboBox1;
   private JTextField textField1;
+  private JLabel runLabel;
+  private JTabbedPane tracePanel;
+  private JTable traceTable;
+  private JPanel graphPanel;
 
   public MyToolWindow(ToolWindow toolWindow) {
     //hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
@@ -38,27 +44,10 @@ public class MyToolWindow {
     backwardSlicerButton.addActionListener(e -> backwardSlicer());
     refreshSlicerButton.addActionListener(e -> refreshSlicer());
     settingsSlicerButton.addActionListener(e -> settingsSlicer());
-  }
+    undoSlicerButton.addActionListener(e -> undoSlicer());
 
-  public void currentDateTime() {
-    // Get current date and time
-    Calendar instance = Calendar.getInstance();
-    currentDate.setText(
-            instance.get(Calendar.DAY_OF_MONTH) + "/"
-                    + (instance.get(Calendar.MONTH) + 1) + "/"
-                    + instance.get(Calendar.YEAR)
-    );
-    //currentDate.setIcon(new ImageIcon(getClass().getResource("/toolWindow/Calendar-icon.png")));
-    int min = instance.get(Calendar.MINUTE);
-    String strMin = min < 10 ? "0" + min : String.valueOf(min);
-    currentTime.setText(instance.get(Calendar.HOUR_OF_DAY) + ":" + strMin);
-    //currentTime.setIcon(new ImageIcon(getClass().getResource("/toolWindow/Time-icon.png")));
-    // Get time zone
-    long gmt_Offset = instance.get(Calendar.ZONE_OFFSET); // offset from GMT in milliseconds
-    String str_gmt_Offset = String.valueOf(gmt_Offset / 3600000);
-    str_gmt_Offset = (gmt_Offset > 0) ? "GMT + " + str_gmt_Offset : "GMT - " + str_gmt_Offset;
-    timeZone.setText(str_gmt_Offset);
-    //timeZone.setIcon(new ImageIcon(getClass().getResource("/toolWindow/Time-zone-icon.png")));
+
+
   }
 
   public void runSlicer(){
@@ -88,59 +77,62 @@ public class MyToolWindow {
     // TODO: place custom component creation code here
     runSlicerButton = new JButton();
     runSlicerButton.setIcon(AllIcons.Actions.Execute);
-    runSlicerButton.setBorderPainted(false);
-    runSlicerButton.setBorder(null);
+    runSlicerButton.setBorderPainted(true);
+    //runSlicerButton.setBorder(null);
     runSlicerButton.setMargin(new Insets(0, 20, 0, 20));
     runSlicerButton.setPreferredSize(new Dimension(20, 20));
     runSlicerButton.setContentAreaFilled(false);
 
     stopSlicerButton = new JButton();
-    stopSlicerButton.setIcon(AllIcons.Process.Stop);
-    stopSlicerButton.setBorderPainted(false);
-    stopSlicerButton.setBorder(null);
+    stopSlicerButton.setIcon(AllIcons.Actions.Suspend);
+    stopSlicerButton.setBorderPainted(true);
+    //stopSlicerButton.setBorder(null);
     stopSlicerButton.setMargin(new Insets(0, 20, 0, 20));
     stopSlicerButton.setPreferredSize(new Dimension(20, 20));
     stopSlicerButton.setContentAreaFilled(false);
 
     backwardSlicerButton = new JButton();
-    backwardSlicerButton.setIcon(AllIcons.Actions.Play_back);
-    backwardSlicerButton.setBorderPainted(false);
-    backwardSlicerButton.setBorder(null);
+    backwardSlicerButton.setIcon(AllIcons.Actions.Back);
+    backwardSlicerButton.setBorderPainted(true);
+    //backwardSlicerButton.setBorder(null);
     backwardSlicerButton.setMargin(new Insets(0, 0, 0, 0));
     backwardSlicerButton.setPreferredSize(new Dimension(20, 20));
     backwardSlicerButton.setContentAreaFilled(false);
 
     forwardSlicerButton = new JButton();
-    forwardSlicerButton.setIcon(AllIcons.Actions.Play_forward);
-    forwardSlicerButton.setBorderPainted(false);
-    forwardSlicerButton.setBorder(null);
+    forwardSlicerButton.setIcon(AllIcons.Actions.Forward);
+    forwardSlicerButton.setBorderPainted(true);
+    //forwardSlicerButton.setBorder(null);
     forwardSlicerButton.setMargin(new Insets(0, 0, 0, 0));
     forwardSlicerButton.setPreferredSize(new Dimension(20, 20));
     forwardSlicerButton.setContentAreaFilled(false);
 
     refreshSlicerButton = new JButton();
     refreshSlicerButton.setIcon(AllIcons.Actions.Refresh);
-    refreshSlicerButton.setBorderPainted(false);
-    refreshSlicerButton.setBorder(null);
+    refreshSlicerButton.setBorderPainted(true);
+    //refreshSlicerButton.setBorder(null);
     refreshSlicerButton.setMargin(new Insets(0, 0, 0, 0));
     refreshSlicerButton.setPreferredSize(new Dimension(20, 20));
     refreshSlicerButton.setContentAreaFilled(false);
 
     settingsSlicerButton = new JButton();
     settingsSlicerButton.setIcon(AllIcons.Actions.Properties);
-    settingsSlicerButton.setBorderPainted(false);
-    settingsSlicerButton.setBorder(null);
+    settingsSlicerButton.setBorderPainted(true);
+    //settingsSlicerButton.setBorder(null);
     settingsSlicerButton.setMargin(new Insets(0, 0, 0, 0));
     settingsSlicerButton.setPreferredSize(new Dimension(20, 20));
     settingsSlicerButton.setContentAreaFilled(false);
 
     undoSlicerButton = new JButton();
     undoSlicerButton.setIcon(AllIcons.Actions.Undo);
-    undoSlicerButton.setBorderPainted(false);
-    undoSlicerButton.setBorder(null);
+    undoSlicerButton.setBorderPainted(true);
+    //undoSlicerButton.setBorder(null);
     undoSlicerButton.setMargin(new Insets(0, 0, 0, 0));
     undoSlicerButton.setPreferredSize(new Dimension(20, 20));
     undoSlicerButton.setContentAreaFilled(false);
+
+    traceTable = new JTable();
+
   }
   private void $$$setupUI$$$() {
     createUIComponents();
